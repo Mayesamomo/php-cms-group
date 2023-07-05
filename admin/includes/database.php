d6d2bfdbf56f1d51b28768d49590ea8e00225bda
Michael A. Momo <mayesamomo1997@gmail.com>
2023-07-05 14:03:46 -0400

    database.php refactored

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,10 +1,15 @@
 <?php
 
-$connect = mysqli_connect( 
-    "<DB_HOST>", // Host
-    "<DB_USER>", // Username
-    "<DB_PASSWORD>", // Password
-    "<DB_DATABASE>" // Database
-);
+// Database connection details
+$host = "localhost"; // address of the database server
+$username = "root"; //  username to access the database
+$password = "";// password to access the database
+$database = "cms"; // name of the  database
 
-mysqli_set_charset( $connect, 'UTF8' );
+// Connect to the database using mysqli_connect function
+$connect = mysqli_connect($host, $username, $password, $database);
+
+// Set the character set of the database connection to UTF8
+mysqli_set_charset($connect, 'UTF8');
+
+
